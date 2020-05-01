57f227e889c446b2b1d38f004e3cba96e9b0aafe
parham <parhamzardoshti@gmail.com>
2020-05-01 19:03:06 +0430

    test on debian systems

--- roles/samba/tasks/AutoSamba.sh
+++ roles/samba/tasks/AutoSamba.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+# Auto Samba Service bash  script
 
 name=$1
 pass=$2
 
 clear
-useradd $name
+if [ ! -f /etc/samba ];then
 
-echo "$pass" | passwd $name --stdin
-echo -e "$pass\n$pass" | smbpasswd -a $name
-echo -e "[$name]\n\tcomment = $name sharing\n\tpath=/home/$name\n\tread only=NO
-	\n\tpublic=yes\n\tguest ok=yes\n\tbrowsable=yes\n\tvalid user=$name\n\t" >> /etc/samba/smb.conf
-clear
-	#config selinux
-setsebool -P samba_enable_home_dirs on
-restorecon -R /home/$name
-ip=$(ifconfig | tail -9 | head -2 | tail -1 | cut -c 14-24)
-echo " this is your $ip for connection"
-echo " make sure that your windows can pings your linux server ip !"
-systemctl restart smb
+	useradd $name
+
+	echo "$pass" | passwd $name --stdin
+	echo -e "$pass\n$pass" | smbpasswd -a $name
+	echo -e "[$name]\n\tcomment = $name sharing\n\tpath=/home/$name\n\tread only=NO
+		\n\tpublic=yes\n\tguest ok=yes\n\tbrowsable=yes\n\tvalid user=$name\n\t" >> /etc/samba/smb.conf
+	clear
+	if [ -f '/etc/selinux/config' ];then
+		setsebool -P samba_enable_home_dirs on
+		restorecon -R /home/$name
+		ip=$(ifconfig | tail -9 | head -2 | tail -1 | cut -c 14-24)
+		echo " this is your $ip for connection"
+		echo " make sure that your windows can pings your linux server ip !"
+		systemctl restart smb
+	else
+		ip=$(ifconfig | grep "inet" | head -1 | cut -c 14-24)
+		echo "you should connect to $ip as your smb server"
+		service smbd restart
+	fi
+else
+	echo "cant install samba"
+fi
